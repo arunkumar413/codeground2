00f94518b28813ca691f83bc0ad6bac8ad0166c8
Arun Kumar <arunkumar413@gmail.com>
2022-11-12 11:48:14 +0000

    searching for libraries in cdnjs working as expec

--- src/modal.js
+++ src/modal.js
@@ -1,22 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { cdnLibraries, modalAtom } from "./appState";
 
 export function AddLibModal() {
   const [libraries, setCdnLibraries] = useRecoilState(cdnLibraries);
-  // const [modalAtom, setModalState] = useRecoilState(modalAtom);
+  const [modalState, setModalState] = useRecoilState(modalAtom);
+  const [searchTerm, setSearchTerm] = useState("");
 
   let libraryElements = libraries.map(function (item, index) {
     return (
-      <div>
-        <div>{item}</div>
+      <div className="modal-search-item" style={{ padding: "0.5rem" }}>
+        <div>{item.name}</div>
       </div>
     );
   });
 
+  function toggleModal() {
+    setModalState(!modalState);
+  }
+
+  function handleSearchChange(evt) {
+    setSearchTerm(evt.target.value);
+  }
+
+  useEffect(
+    function () {
+      //fetchlibraries
+      fetch(`https://api.cdnjs.com/libraries?search=${searchTerm}`)
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          console.log(data.results);
+          setCdnLibraries(data.results);
+        });
+    },
+    [searchTerm]
+  );
+
   return (
-    <div className="add-lib-modal">
-      <p> Modal</p>
+    <div
+      className="add-lib-modal"
+      style={{ display: modalState === true ? "block" : "none" }}
+    >
+      <div className="modal-header">
+        <h5>Search for a library</h5>
+        <span onClick={toggleModal} className="modal-header-close-button ">
+          {" "}
+          x{" "}
+        </span>{" "}
+      </div>
+      <div>
+        <input
+          name="search-library"
+          onChange={handleSearchChange}
+          type="text"
+        />
+      </div>
+      {libraryElements}
     </div>
   );
 }
